d9e8be6d855a28a0b17696541895927f4144998a
petartasic <petartasic@hotmail.com>
2014-12-22 06:32:20 +0000

    adjusted

--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Lead, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @lead = Lead.create!(name: "Sam", company: "ABC Company")
+  end
+  it "should create a new lead" do
+    expect(Lead.count).to eq(1)
+  end
+  
 end
